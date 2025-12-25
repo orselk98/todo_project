c6f305739103f972dc8a83bb49bd7461140ace21
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2025-12-25 23:08:52 +0100

    added the rest of http methods and ran some tests

--- tasks/views.py
+++ tasks/views.py
@@ -8,7 +8,7 @@ import json
 @csrf_exempt
 def task_list(request):
     if request.method == 'GET':
-        tasks_qs = Task.objects.all.values('id', 'title', 'completed', 'created_at')
+        tasks_qs = Task.objects.all().values('id', 'title', 'completed', 'created_at')
         #converting querylist to list
         tasks_list = list(tasks_qs)
         return JsonResponse (tasks_list, safe=False)
@@ -43,3 +43,60 @@ def task_list(request):
             }
         } , status = 201)
     return JsonResponse ({'error' : 'Method not allowed'}, status = 405)
+
+#Get , Patch , Delete a single task
+
+@csrf_exempt
+def task_detail (request, pk):
+    #First lets try to find the task
+    try:
+        task = Task.objects.get(id=pk)
+    except Task.DoesNotExist:
+        return JsonResponse ({'error':'Task Not Found'}, status =404)
+    
+    if request.method =='GET':
+        #Return the single task
+        task_dict ={
+            'id' : task.id,
+            'title':task.title,
+            'completed' : task.completed,
+            'created_at' : task.created_at.isoformat()
+        }
+        return JsonResponse(task_dict, safe=False)
+    
+    if request.method == 'PATCH':
+        #get data from request body
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse ({'error': 'Invalid JSON'},status=400)
+        
+        #extract the files
+        title = data.get('title')
+        completed =data.get('completed')
+
+        #update the task fields
+        if title:
+            task.title = title
+        if completed is not None: #check for none because false is avalid value
+            task.completed= completed
+
+        #save to db
+        task.save()
+
+        #Return success message
+        return JsonResponse ({
+            'message' : 'Task updated sucessfully',
+            'task' : {
+                'id' : task.id,
+                'title' : task.title,
+                'completed' : task.completed,
+                'created_at' : task.created_at.isoformat()
+            }
+        })
+    if request.method == 'DELETE':
+        #delete the task
+        task.delete()
+        return JsonResponse({'message' : 'Task deleted successfully'}, status =204)
+    
+    return JsonResponse({'error' : 'Method not allowed'}, status =405)
